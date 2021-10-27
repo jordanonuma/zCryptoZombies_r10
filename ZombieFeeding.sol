4f2afe18e9fef4c5f267c11e301e237335541cf2
jordanonuma <jordanonuma@outlook.com>
2021-10-26 19:44:22 -1000

    Added frame of interface to interact with crypto kitties contract.

--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -1,6 +1,9 @@
 pragma solidity >=0.5.0 <0.6.0;
 import "./ZombieFactory.sol";
 
+contract KittyInterface {
+    
+} //end KittyInterface{}
 contract ZombieFeeding is ZombieFactory {
     function feedAndMultiply(uint _zombieId, uint _targetDna) public {
         require(msg.sender == zombieToOwner[_zombieId]);
