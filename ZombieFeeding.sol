b457d8e4a1cb4d65940f817ae1aaa7f3d89a9856
jordanonuma <jordanonuma@outlook.com>
2021-10-26 19:46:38 -1000

    Specified the getKitty() function.

--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -2,8 +2,20 @@ pragma solidity >=0.5.0 <0.6.0;
 import "./ZombieFactory.sol";
 
 contract KittyInterface {
-    
+    function getKitty(uint256 _id) external view returns (
+        bool isGestating,
+        bool isReady,
+        uint256 cooldownIndex,
+        uint256 nextActionAt,
+        uint256 siringWithId,
+        uint256 birthTime,
+        uint256 matronId,
+        uint256 sireId,
+        uint256 generation,
+        uint256 genes
+    );
 } //end KittyInterface{}
+
 contract ZombieFeeding is ZombieFactory {
     function feedAndMultiply(uint _zombieId, uint _targetDna) public {
         require(msg.sender == zombieToOwner[_zombieId]);
