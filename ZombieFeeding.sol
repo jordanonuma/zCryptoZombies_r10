08d26607276dffb91f496c90f6629f8f25c3312b
jordanonuma <jordanonuma@outlook.com>
2021-10-29 20:48:20 -1000

    Passes the species as a definer of new zombie. Checks of this is a zombie born from a crypto kitty as one of the parents.

--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -20,12 +20,16 @@ contract ZombieFeeding is ZombieFactory {
     address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
     KittyInterface kittyContract = KittyInterface(ckAddress); //sets up a KittyInterface called 'kittyContract'
 
-    function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+    function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
         require(msg.sender == zombieToOwner[_zombieId]);
         Zombie storage myZombie = zombies[_zombieId]; //array 'myZombie' of structs type Zombie[]
 
         _targetDna = _targetDna % dnaModulus;
         uint newDna = (myZombie.dna + _targetDna) / 2;
+
+        if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))) {
+        
+        } //end if()
         _createZombie("NoName", newDna);
     } //end function feedAndMultiply()
 
