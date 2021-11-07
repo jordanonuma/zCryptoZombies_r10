db6edc0c4ad92fc278bd346128527d98a35219ae
jordanonuma <jordanonuma@outlook.com>
2021-11-07 09:19:47 -1000

    Revised variable name for function-specific variables and uint type.

--- ZombieHelper.sol
+++ ZombieHelper.sol
@@ -8,13 +8,13 @@ contract ZombieHelper is ZombieFeeding {
         _;
     } //end modifier aboveLevel()
 
-    function changeName(uint zombieId, string calldata _newName) external aboveLevel(2, _zombieId) {
+    function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId) {
         require(zombieToOwner[_zombieId] == msg.sender);
         zombies[_zombieId].name = _newName;
     } //end function changeName()
 
-    function changeDna(uint zombieId, string calldata _newName) external aboveLevel(20, _zombieId) {
-        require(zombieToOwner[zombieId] == msg.sender);
+    function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) {
+        require(zombieToOwner[_zombieId] == msg.sender);
         zombies[_zombieId].dna = _newDna;
     } //end function changeDna()
 } //end contract ZombieHelper{}
