cc3111ce449292d5131569a49c664dfe721a4be3
jordanonuma <jordanonuma@outlook.com>
2021-11-14 10:16:08 -1000

    Added frame of function that takes the modulus of a pseudo-random number.

--- ZombieAttack.sol
+++ ZombieAttack.sol
@@ -2,5 +2,8 @@ pragma solidity >=0.5.0 <0.6.0;
 import "./zombiehelper.sol";
 
 contract ZombieAttack is ZombieHelper {
-  
+    uint randNonce = 0;
+    function randMod(uint _modulus) internal returns(uint) {
+    
+    } //end function randMod()
 } //end contract ZombieAttack{}
