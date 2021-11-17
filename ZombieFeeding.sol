034bc679e660cb4e32ac9a64b82ada086590195b
jordanonuma <jordanonuma@outlook.com>
2021-11-16 20:44:38 -1000

    Applies modifier and deletes redundant code in body.

--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -37,8 +37,8 @@ contract ZombieFeeding is ZombieFactory {
         return (_zombie.readyTime <= now);
     } //end function _isReady()
 
-    function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) internal {
-        require(msg.sender == zombieToOwner[_zombieId]);
+    function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) internal ownerOf(_zombieId) {
+        
         Zombie storage myZombie = zombies[_zombieId]; //array 'myZombie' of structs type Zombie[]
         require(_isReady(myZombie));
 
