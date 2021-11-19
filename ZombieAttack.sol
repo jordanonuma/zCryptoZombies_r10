edb8a02b5133c369622955989f46fdb7abec9fcc
jordanonuma <jordanonuma@outlook.com>
2021-11-18 18:19:32 -1000

    Defined attacking and defensive zombie.

--- ZombieAttack.sol
+++ ZombieAttack.sol
@@ -10,7 +10,8 @@ contract ZombieAttack is ZombieHelper {
         return uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % _modulus;
     } //end function randMod()
 
-    function attack(uint _zombieId, uint _targetId) external {
-    
+    function attack(uint _zombieId, uint _targetId) external ownerOf(_zombieId) {
+        Zombie storage myZombie = zombies[_zombieId];
+        Zombie storage enemyZombie = zombies[_targetId];
     } //end function attack()
 } //end contract ZombieAttack{}
