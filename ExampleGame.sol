b3a5babb4a824fd6a31d549c08ce62fdc6964d15
jordanonuma <jordanonuma@outlook.com>
2021-12-20 07:27:09 -1000

    Initializes the card counter to 0.

--- ExampleGame.sol
+++ ExampleGame.sol
@@ -53,6 +53,7 @@ contract ExampleGame is ZBGameMode  {
 
         for (uint i = 0; i< gameState.playerStates.length; i++) {
             CardInstances newCards = gameState.playerStates[i].cardsInDeck.length;
+            uint cardCount = 0;
         } //end for()
     } //end function beforeMatchStart()
 
