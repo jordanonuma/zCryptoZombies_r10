3877ae12586d36fc5ad8217bc179c841796843c8
jordanonuma <jordanonuma@outlook.com>
2021-12-21 09:33:51 -1000

    Closed parentheses for syntax.

--- ExampleGame.sol
+++ ExampleGame.sol
@@ -55,7 +55,7 @@ contract ExampleGame is ZBGameMode  {
             CardInstance[] memory newCards = new CardInstance[](gameState.playerStates[i].cardsInDeck.length);
             uint cardCount = 0;
             for (uint j = 0; j < gameState.playerStates[i].cardsInDeck.length; j++) {
-                if (isLegalCard(gameState.playerStates[i].cardsInDeck[j]) {
+                if (isLegalCard(gameState.playerStates[i].cardsInDeck[j])) {
                     newCards[cardCount] = gameState.playerStates[i].cardsInDeck[j];
                     cardCount++;
                 } //end if()
