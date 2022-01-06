83a61d797d7471570853e7f0790dee47a3e6c18b
jordanonuma <jordanonuma@outlook.com>
2022-01-05 18:21:57 -1000

    Declared Interface variable 'priceFeed' and instantiate it passing a pre-defined contract address.

--- Chainlink/Contract.sol
+++ Chainlink/Contract.sol
@@ -2,4 +2,9 @@ pragma solidity ^0.6.7;
 
 contract PriceConsumerV3 {
 
+    AggregatorV3Interface priceFeed;
+    Constructor() {
+        priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+    } //end Constructor()
+
 } //end contract PriceConsumerV3{}
