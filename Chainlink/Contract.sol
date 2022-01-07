e0d7b28be83fd5b3d85f0eeb269a9af2d445969b
jordanonuma <jordanonuma@outlook.com>
2022-01-06 17:50:44 -1000

    Added frame of function that calls the getLatestPrice() function from the priceFeed{} contract.

--- Chainlink/Contract.sol
+++ Chainlink/Contract.sol
@@ -7,4 +7,8 @@ contract PriceConsumerV3 {
         priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
     } //end constructor()
 
+    function getLatestPrice() public view returns(uint) {
+      
+    } //end function getLatestPrice()
+
 } //end contract PriceConsumerV3{}
