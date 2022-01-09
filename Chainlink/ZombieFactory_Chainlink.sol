d401a3428a6c2786570a789fd1e16c5edad07424
jordanonuma <jordanonuma@outlook.com>
2022-01-09 07:59:27 -1000

    Uploaded ZombieFactory{} for ChainLink oracle.

---
+++ Chainlink/ZombieFactory_Chainlink.sol
@@ -0,0 +1,47 @@
+pragma solidity >=0.5.0 <0.6.0;
+import "./Ownable.sol";
+import "./SafeMath.sol";
+
+contract ZombieFactory is Ownable {
+    using SafeMath for uint256;
+    using SafeMath32 for uint32;
+    using SafeMath16 for uint16;
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+    uint cooldownTime = 1 days;
+
+    event NewZombie(uint zombieId, string name, uint dna);
+
+    struct Zombie {
+        string name;
+        uint dna;
+        uint32 level;
+        uint32 readyTime;
+        uint16 winCount;
+        uint16 lossCount;
+    } //end struct Zombie{}
+
+    Zombie[] public zombies;
+
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) public ownerZombieCount;
+
+    function _createZombie(string memory _name, uint _dna) internal {
+        uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime), 0, 0)) - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender] = ownerZombieCount[msg.sender].add(1);
+        emit NewZombie(id, _name, _dna);
+    } //end function _createZombie()
+
+    function _generateRandomDna(string memory _str) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    } //end function _generateRandomDna()
+
+    function createRandomZombie(string memory _name) public {
+        require(ownerZombieCount[msg.sender] == 0);
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    } //end function createRandomZombie()
+} //end contract ZombieFactory{}
