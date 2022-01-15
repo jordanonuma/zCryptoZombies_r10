4b1d5f2bd550ac6c2894c9b06fe4d0fc4031237b
jordanonuma <jordanonuma@outlook.com>
2022-01-15 11:23:02 -1000

    Initialized zombie names.

--- Test/CryptoZombies.js
+++ Test/CryptoZombies.js
@@ -1,4 +1,5 @@
 const CryptoZombies = artifacts.require("CryptoZombies");
+const zombieNames = ["Zombie 1", "Zombie 2"];
 
 contract("CryptoZombies", (accounts) => {
     let [alice, bob] = accounts;
