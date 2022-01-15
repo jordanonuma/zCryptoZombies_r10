14131264b45ea5eb24844ef4b281a9460a9ff7f2
jordanonuma <jordanonuma@outlook.com>
2022-01-14 19:02:17 -1000

    Prepending with 'async' so contract will know to wait for tests that are prepended with 'await'.

--- Test/CryptoZombies.js
+++ Test/CryptoZombies.js
@@ -2,8 +2,8 @@ const CryptoZombies = artifacts.require("CryptoZombies");
 
 contract("CryptoZombies", (accounts) => {
     let [alice, bob] = accounts;
-    
-    it("should be able to create a new zombie", () => {
+
+    it("should be able to create a new zombie", async () => {
         
     })
 }) //end contract()
