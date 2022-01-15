d0e7c89dc4ad3189b65574eb3dce922ea81acb73
jordanonuma <jordanonuma@outlook.com>
2022-01-15 11:24:23 -1000

    Added contract instance of CryptoZombies.

--- Test/CryptoZombies.js
+++ Test/CryptoZombies.js
@@ -5,6 +5,6 @@ contract("CryptoZombies", (accounts) => {
     let [alice, bob] = accounts;
 
     it("should be able to create a new zombie", async () => {
-        
+        const contractInstance = await CryptoZombies.new();
     })
 }) //end contract()
