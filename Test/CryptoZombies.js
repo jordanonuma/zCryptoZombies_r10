e85ecf15ef6c5b671d1790004227b862b18e72f6
jordanonuma <jordanonuma@outlook.com>
2022-01-16 08:35:47 -1000

    Checks if the transaction was successful and the name of the sent zombie was correct.

--- Test/CryptoZombies.js
+++ Test/CryptoZombies.js
@@ -6,5 +6,8 @@ contract("CryptoZombies", (accounts) => {
 
     it("should be able to create a new zombie", async () => {
         const contractInstance = await CryptoZombies.new();
+        const result = await contractInstance.createRandomZombie(zombieNames[0], {from:  alice});
+        assert.equal(result.receipt.status, true);
+        assert.equal(result.logs[0].args.name, zombieNames[0]);
     })
 }) //end contract()
