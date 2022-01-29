c32906c121a30809ba0444dc448691e69f2cd4cc
jordanonuma <jordanonuma@outlook.com>
2022-01-29 08:15:23 -1000

    Defined the oracle's address and created function that allows owner to set the address.

--- Caller/CallerContract.sol
+++ Caller/CallerContract.sol
@@ -1,5 +1,9 @@
 pragma solidity 0.5.0;
 
 contract CallerContract {
-    
+    address private oracleAddress;
+
+    function setOracleInstanceAddress(address _oracleInstanceAddress) public {
+        oracleAddress = _oracleInstanceAddress;
+    } //end function setOracleInstanceAddress()
 } //end contract CallerContract{}
