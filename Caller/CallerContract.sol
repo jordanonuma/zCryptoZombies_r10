4520ce91dbc1a3d092d3babee90ade20d06dfdfc
jordanonuma <jordanonuma@outlook.com>
2022-01-30 07:45:37 -1000

    Imports the interfacing contract and instantiates the interface with the contract address.

--- Caller/CallerContract.sol
+++ Caller/CallerContract.sol
@@ -1,9 +1,12 @@
 pragma solidity 0.5.0;
+import "./EthPriceOracleInterface.sol";
 
 contract CallerContract {
     address private oracleAddress;
+    EthPriceOracleInterface private oracleInstance;
 
     function setOracleInstanceAddress(address _oracleInstanceAddress) public {
         oracleAddress = _oracleInstanceAddress;
+        oracleInstance = EthPriceOracleInterface(oracleAddress);
     } //end function setOracleInstanceAddress()
 } //end contract CallerContract{}
