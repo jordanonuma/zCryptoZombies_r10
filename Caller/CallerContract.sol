17dd06683b44e7d52768dfe441d0229ef2b2b0e8
jordanonuma <jordanonuma@outlook.com>
2022-02-01 17:29:07 -1000

    Defined request mapping and event when a request has been received.

--- Caller/CallerContract.sol
+++ Caller/CallerContract.sol
@@ -4,12 +4,16 @@ import "./Ownable.sol";
 
 contract CallerContract is Ownable {
     address private oracleAddress;
+    mapping(uint256=>bool) myRequests;
     EthPriceOracleInterface private oracleInstance;
     event newOracleAddressEvent(address oracleAddress);
+    event ReceivedNewRequestIdEvent(uint256 id);
 
     function setOracleInstanceAddress(address _oracleInstanceAddress) public onlyOwner {
         oracleAddress = _oracleInstanceAddress;
         oracleInstance = EthPriceOracleInterface(oracleAddress);
         emit newOracleAddressEvent(oracleAddress);
     } //end function setOracleInstanceAddress()
+
+
 } //end contract CallerContract{}
