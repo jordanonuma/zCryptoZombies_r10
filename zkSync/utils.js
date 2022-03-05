a6401df1e203fa44cc0088ebacc8a3b6eb8b6dcb
jordanonuma <jordanonuma@outlook.com>
2022-03-04 16:29:42 -1000

    Returns provider within function.

--- zkSync/utils.js
+++ zkSync/utils.js
@@ -1,10 +1,10 @@
 async function getZkSyncProvider(zksync, networkName) {
-    let getZkSyncProvider
+    let ZkSyncProvider
     try {
         zkSyncProvider = await zksync.getDefaultProvider(networkName)
     } catch (error) {
         console.log('Unable to connect to zkSync.')
         console.log(error)
     }
+    return zkSyncProvider
 } //end function getZkSyncProvider()
-return zkSyncProvider
