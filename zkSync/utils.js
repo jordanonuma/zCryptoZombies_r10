77a22e31475ff1aa00d65de7845c1b37ca1041b4
jordanonuma <jordanonuma@outlook.com>
2022-03-06 07:30:41 -1000

    Verifies a signing key has not been set.

--- zkSync/utils.js
+++ zkSync/utils.js
@@ -28,6 +28,8 @@ async function initAccount(rinkebyWallet, zkSyncProvider, zksync) {
 
 async function registerAccount (wallet) {
     console.log(`Registering the ${wallet.address()} account on zkSync`)
-    
+    if (!await wallet.isSigningKeySet()) {
+      
+    } //end if()
 
 } //end function registerAccount()  
