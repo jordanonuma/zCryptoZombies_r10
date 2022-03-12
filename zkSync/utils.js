b2c7ba232ecdcbb8ccf2e2166298341eb67f7be5
jordanonuma <jordanonuma@outlook.com>
2022-03-12 08:20:01 -1000

    Modules.exports specifies functions and variables Node.js will export from utils.js

--- zkSync/utils.js
+++ zkSync/utils.js
@@ -98,8 +98,20 @@ async function displayZkSyncBalance (wallet, ethers) {
     } //end if-else{}
 
     if (state.verified.balances.ETH) {
-        console.log(`Commited ETH balance for ${wallet.address()}: ${ethers.utils.formatEther(state.verified.balances.ETH)}`)
+        console.log(`Verified ETH balance for ${wallet.address()}: ${ethers.utils.formatEther(state.verified.balances.ETH)}`)
     } else {
         console.log(`Verified ETH balance for ${wallet.address()}: 0`)
     } //end if-else{}
 } //end function displayZkSyncBalance()
+
+module.exports = {
+    getZkSyncProvider,
+    getEthereumProvider,
+    depositToZkSync,
+    registerAccount,
+    displayZkSyncBalance,
+    transfer,
+    withdrawToEthereum,
+    getFee,
+    initAccount
+}
