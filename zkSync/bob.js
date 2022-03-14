dbd782b97fceb87e7979363bd6a5e8b72f9110e0
jordanonuma <jordanonuma@outlook.com>
2022-03-14 07:00:35 -1000

    Create's Bob's zkSync wallet.

--- zkSync/bob.js
+++ zkSync/bob.js
@@ -9,4 +9,5 @@
     const bobRinkebyWallet = new ethers.Wallet(process.env.BOB_PRIVATE_KEY, ethersProvider)
     console.log(`Bob's Rinkeby address is: ${bobRinkebyWallet.address}`)
     console.log(`Bob's initial balance on Rinkeby is: ${ethers.utils.formatEther(await bobRinkebyWallet.getBalance())}`)
+    const bobZkSync = await utils.initAccount(bobRinkebyWallet, zkSyncProvider, zksync)
 })()
